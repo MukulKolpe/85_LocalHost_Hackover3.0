2cfe0cde1d1e34d6f19eebb75ab4da1530bb3132
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-10-05 01:32:49 +0530

    Added Footer + Hero Component🚀

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,47 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <div className="max-w-xl mb-6">
+            <h2 className="text-center max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Eventify
+              <br className="text-center hidden md:block" />
+              Events Brought
+              <br className="hidden md:block" />
+              <span className="relative px-1">
+                <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
+                <span className="text-center relative inline-block text-deep-purple-accent-400">
+                  Closer To You
+                </span>
+              </span>
+            </h2>
+            <p className="md:text-lg text-slate-700 text-bold">
+              Eventify is a platform that allows you to find events near you and
+              also allows you to create events.
+            </p>
+            <div className="mb-10 mt-5 text-center md:mb-16 lg:mb-20">
+              <a
+                href="/"
+                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              >
+                Book Now
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+            src="https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
