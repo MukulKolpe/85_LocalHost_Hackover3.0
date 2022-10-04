5e30f563dd5ac48b42952ca8533fa05bc241f6b6
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-04 20:48:27 +0530

    basic organizers backend setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+var cors = require('cors');
+const express = require('express');
+const app = express();
+
+//middleware
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
+
+//db
+const connectDB = require('./db/connect')
+
+//routes
+const organizerRoutes = require('./routes/organizer')
+
+
+app.use(express.json());
+app.use(cors())
+app.get('/api', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+  })
+
+app.use('/api/organizers',organizerRoutes)
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try{
+       await connectDB(process.env.MONGODB_URI)
+      app.listen(port , console.log(`Server started at port ${port}`))
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+start();
+
---
+++ backend/middleware/not-found.js
@@ -0,0 +1,3 @@
+const notFound = (req, res) => res.status(404).send('Page not found')
+
+module.exports = notFound
