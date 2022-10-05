849e0b1896f350fb45fca31727157c796b1cc2b8
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-05 08:22:14 +0530

    follow events backend done

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -5,6 +5,22 @@ const getUsers = async (req, res) => {
     res.status(200).json(await User.find())
 }
 
+const followEvents = asyncHandler(async (req, res) => {
+    const { email , followedEvent } = req.body
+    const favItem = await User.findOneAndUpdate({email},{$push:{followedEvents:followedEvent}},{new:true})
+          res.status(201).json(favItem)
+
+})
+
+const userbyemail = async (req, res) => {
+    const { email } = req.params
+    User.findOne({ email:email})
+      .populate('followedEvents').exec((err, doc) => {
+        if (err) { return console.error(err); }
+         res.status(200).json(doc)
+    })
+}
+
 const addUser = asyncHandler(async (req, res) => {
     const {name,email,image,followedEvents} = req.body;
     const userExist = await User.findOne({email})
@@ -32,4 +48,4 @@ const addUser = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { getUsers, addUser }
+module.exports = { getUsers, addUser, followEvents ,userbyemail} 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {getUsers,addUser} = require('../controller/user');
+const {getUsers,addUser,followEvents , userbyemail} = require('../controller/user');
 
 router.route('/').get(getUsers)
 router.route('/add').post(addUser)
-
+router.route('/follow').put(followEvents)
+router.route('/:email').get(userbyemail)
 module.exports = router;
